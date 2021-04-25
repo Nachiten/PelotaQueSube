7d51380945f8296288bc5cd10a41978584681451
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-04-24 23:32:26 -0300

    Optimizacion de codigo

--- Assets/Scripts/BolaManager.cs
+++ Assets/Scripts/BolaManager.cs
@@ -1,15 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class MovimientoBola : MonoBehaviour
+public class BolaManager : MonoBehaviour
 {
-    
+    bool perdio = false;
 
-    // Update is called once per frame
+    void perdioJuego()
+    {
+        //Debug.Log("Perdio Juego!");
+        GameObject.Find("GameManager").GetComponent<GameManager>().perderJuego();
+
+        perdio = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("[ColisionBola] OnColisionEnter2D");
+
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.tag == "Obstaculo")
+            perdioJuego();
+    }
+
     void Update()
     {
+        if (perdio)
+            return;
+        
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
